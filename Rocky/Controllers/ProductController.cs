bc6a27ea584de7cb596c08cae80edef48e6ebf4a
Vitalii Matvieiev <Vitaliim@mwstechnology.com>
2021-08-07 21:46:03 +0300

    add product and product functionslity

---
+++ Rocky/Controllers/ProductController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Rocky.Data;
+using Rocky.Models;
+using Rocky.Models.ViewModels;
+
+namespace Rocky.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webHostEnviromnet;
+
+        public ProductController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
+        {
+            _db = db;
+            _webHostEnviromnet = webHostEnvironment;
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objList = _db.Product;
+            foreach (var obj in objList)
+            {
+                obj.Category = _db.Category.FirstOrDefault(u => u.Id == obj.CategoryId);
+            }
+            return View(objList);
+        }
+
+
+        //GET - UPSERT
+        public IActionResult Upsert(int? id)
+        {
+            ProductVM productVM = new ProductVM()
+            {
+                Product = new Product(),
+                CategorySelectionList = _db.Category.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+            };
+
+            if (id == null)
+            {
+                //this for create
+                return View(productVM);
+            } 
+            else
+            {
+                productVM.Product = _db.Product.Find(id);
+                if (productVM.Product == null)
+                {
+                    return NotFound();
+                }
+                return View(productVM);
+            }
+        }
+
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductVM productVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                string webRootPath = _webHostEnviromnet.WebRootPath;
+
+                 if (productVM.Product.Id == 0)
+                {
+                    string upload = webRootPath + WC.ImagePath;
+                    string fileName = Guid.NewGuid().ToString();
+                    string extension = Path.GetExtension(files[0].FileName);
+
+                    using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStream);
+                    }
+
+                    productVM.Product.Image = fileName + extension;
+                    _db.Product.Add(productVM.Product);
+                }
+                else
+                {
+                    var objFromDb = _db.Product.AsNoTracking().FirstOrDefault(u => u.Id == productVM.Product.Id);
+
+                    if(files.Count > 0)
+                    {
+                        string upload = webRootPath + WC.ImagePath;
+                        string fileName = Guid.NewGuid().ToString();
+                        string extension = Path.GetExtension(files[0].FileName);
+
+                        var oldFile = Path.Combine(upload, objFromDb.Image);
+
+                        if (System.IO.File.Exists(oldFile))
+                        {
+                            System.IO.File.Delete(oldFile);
+                        }
+
+                        using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                        {
+                            files[0].CopyTo(fileStream);
+                        }
+
+                        productVM.Product.Image = fileName + extension;
+
+                    }
+                    else
+                    {
+                        productVM.Product.Image = objFromDb.Image;
+                    }
+                    _db.Product.Update(productVM.Product);
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index"); 
+            }
+            productVM.CategorySelectionList = _db.Category.Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
+            return View(productVM);
+        }
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            ProductVM productVM = new ProductVM();
+            
+            productVM.Product = _db.Product.Include(u=>u.Category).FirstOrDefault(u=>u.Id==id);
+            /*productVM.Product.Category = _db.Category.Find(productVM.Product.CategoryId);*/
+            if (productVM == null)
+            {
+                return NotFound();
+            }
+
+            return View(productVM);
+        }
+        //POST - DELETE
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            string webRootPath = _webHostEnviromnet.WebRootPath;
+
+            var product = _db.Product.Find(id);
+
+            string upload = webRootPath + WC.ImagePath;
+            var oldFile = Path.Combine(upload, product.Image);
+
+            if (System.IO.File.Exists(oldFile))
+            {
+                System.IO.File.Delete(oldFile);
+            }
+
+            _db.Product.Remove(product);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
